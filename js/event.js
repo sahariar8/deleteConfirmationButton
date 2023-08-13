4fbab52da4cfe8d1c66d33b1c8b9124913e5521b
Sahariar <sahariaralam8@gmail.com>
2023-08-13 17:42:12 +0600

    complt

--- js/event.js
+++ js/event.js
@@ -1,1 +1,19 @@
-console.log("hello js")
+document.getElementById("text").addEventListener('keyup',function(event){
+    const text = event.target.value;
+    console.log(text);
+    const button = document.getElementById('btn-delete');
+    if(text === "delete"){
+        button.removeAttribute('disabled')
+    }else{
+        button.setAttribute('disabled',true);
+    }
+    button.addEventListener('click',function(){
+        const hide =document.getElementById('secret');
+        hide.style.display = "none"
+        const dele = document.getElementById('text')
+        dele.value = '';
+        
+    
+       
+    })
+})
